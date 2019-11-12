1f55cabdc4811b3316d6090e78938cb8810c8039
yongbowin <yongbowin@outlook.com>
2019-11-12 18:24:20 +0800

    add annotation on preprocess

--- prepro_utils.py
+++ prepro_utils.py
@@ -12,35 +12,38 @@ class IdxTag_Converter(object):
     ''' idx2tag : a tag list like ['O','B','I','E','U']
         tag2idx : {'O': 0, 'B': 1, ..., 'U':4}
     '''
+
     def __init__(self, idx2tag):
         self.idx2tag = idx2tag
         tag2idx = {}
         for idx, tag in enumerate(idx2tag):
             tag2idx[tag] = idx
         self.tag2idx = tag2idx
-        
+
     def convert_idx2tag(self, index_list):
         tag_list = [self.idx2tag[index] for index in index_list]
         return tag_list
-        
+
     def convert_tag2idx(self, tag_list):
         index_list = [self.tag2idx[tag] for tag in tag_list]
         return index_list
+
 
 # ----------------------------------------------------------------------------------------
 # delet null keyphrase
 # ----------------------------------------------------------------------------------------
 class DEL_ASCII(object):
     ''' used in function `tokenize_data` for filter ASCII character b'\xef\xb8\x8f' '''
+
     def do(self, text):
         orig_tokens = self.whitespace_tokenize(text)
         split_tokens = []
         for token in orig_tokens:
-            token = self._run_strip_accents(token)                
+            token = self._run_strip_accents(token)
             split_tokens.extend(self._run_split_on_punc(token))
         output_tokens = self.whitespace_tokenize(" ".join(split_tokens))
         return output_tokens
-    
+
     def whitespace_tokenize(self, text):
         """Runs basic whitespace cleaning and splitting on a piece of text."""
         text = text.strip()
@@ -59,7 +62,7 @@ class DEL_ASCII(object):
                 continue
             output.append(char)
         return "".join(output)
-    
+
     def _run_split_on_punc(self, text):
         """Splits punctuation on a piece of text."""
         chars = list(text)
@@ -78,7 +81,7 @@ class DEL_ASCII(object):
                 output[-1].append(char)
             i += 1
         return ["".join(x) for x in output]
-    
+
     def _is_punctuation(self, char):
         """Checks whether `chars` is a punctuation character."""
         cp = ord(char)
@@ -93,7 +96,8 @@ class DEL_ASCII(object):
         if cat.startswith("P"):
             return True
         return False
-        
+
+
 # ----------------------------------------------------------------------------------------
 # find keyphrase position in document and merge the same keyphrases
 # ----------------------------------------------------------------------------------------
@@ -107,12 +111,18 @@ def merge_same(phrases):
         lower_phrases : uncased phrase token list ,
         used for find keyphrase positions in document . 
         prepro_phrases : keep cased keyphrase (remove '.' in keyphrase's tail )
+
+    phrases :
+        [[kp_w1, kp_w2, ...], ...]
+
+    lower_phrases (lower), prepro_phrases (raw) format :
+        [[kp_w1, kp_w2, ...], ...]
     '''
     phrases_set = set()
     lower_phrases = []
     prepro_phrases = []
     for phrase in phrases:
-        phrase_string = ' '.join(phrase).strip('.').strip(' ') # delete '.' in the tail
+        phrase_string = ' '.join(phrase).strip('.').strip(' ')  # delete '.' in the tail
         lower_phrase = phrase_string.lower()
 
         if lower_phrase in phrases_set:
@@ -121,8 +131,8 @@ def merge_same(phrases):
         lower_phrases.append(lower_phrase.split())
         prepro_phrases.append(phrase_string.split())
     return lower_phrases, prepro_phrases
-    
-    
+
+
 def filter_overlap(positions):
     '''delete overlap keyphrase positions'''
     previous_e = -1
@@ -148,35 +158,50 @@ def find_answer(document, answers):
         a present answer postions list : every present's positions in documents, 
         each answer can be presented in several postions .
         [[[0,0],[20,21]], [[1,1]]]
+
+    document:
+        [w1, w2, w3, ...]
+    answers:
+        [[kp_w1, kp_w2, ...], ...]
     '''
     tot_doc_char = ' '.join(document)
-    
+
+    """
+    :return
+        `positions_for_all`, (all position of each `keyphrase` in current text.)
+        format:
+            [ [[s1, e1], [s2, e2], ...], [[s1, e1], [s2, e2], ...], ... ]
+                        keyphrase1                  keyphrase2      ...
+        
+        `all_present_answers`, (all answers exist in current text.)
+            [answer1 str, answer2 str, ...]
+    """
     positions_for_all = []
     all_present_answers = []
     for answer in answers:
         ans_string = ' '.join(answer)
-        
+
         if ans_string not in tot_doc_char:
             continue
-        else: 
-            positions_for_each = []
+        else:
+            positions_for_each = []  # 找出当前keyphrase在text中的所有位置, [[s1, e1], [s2, e2], ...]
             # find all positions for each answer
             for i in range(0, len(document) - len(answer) + 1):
                 Flag = False
-                if answer == document[i:i+len(answer)]:
+                if answer == document[i:i + len(answer)]:
                     Flag = True
                 if Flag:
-                    assert (i+len(answer)-1) >= i
-                    positions_for_each.append([i, i+len(answer)-1])
-        if len(positions_for_each) > 0 :
+                    assert (i + len(answer) - 1) >= i
+                    positions_for_each.append([i, i + len(answer) - 1])
+        if len(positions_for_each) > 0:
             positions_for_all.append(positions_for_each)
             all_present_answers.append(answer)
-            
+
     assert len(positions_for_all) == len(all_present_answers)
-    
+
     if len(all_present_answers) == 0:
         return None
-    return {'keyphrases':all_present_answers, 'start_end_pos':positions_for_all}
+    return {'keyphrases': all_present_answers, 'start_end_pos': positions_for_all}
 
 
 # ----------------------------------------------------------------------------------------
@@ -190,6 +215,7 @@ def verify_ex(ex):
         return True
     return False
 
+
 def check_tokenize(ex):
     if len(ex['tokens']) != len(ex['valid_mask']):
         logger.info('ERROR : lengths not equal between tokens & valid_mask !')
@@ -198,6 +224,7 @@ def check_tokenize(ex):
         logger.info('ERROR : lengths not equal between sum_of valid_mask & orig_tokens !')
         return False
     return True
+
 
 def check_tag(ex):
     if sum(ex['valid_mask']) != len(ex['label']):
@@ -210,6 +237,8 @@ def check_tag(ex):
         logger.info('ERROR : sorted_positions not equal orig_start_end_pos!')
         return False
     return True
+
+
 # ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
 # convert tag list to start and end positions for (verify & evaluate)
@@ -233,13 +262,13 @@ def get_chunk_positions(tag_list, score_list=None):
     chunk_scores = []
     while abs_p1 < len(tag_list):
         now_tag = tag_list[abs_p1]
-        
+
         if now_tag not in ['U', 'B']:
             abs_p1 += 1
             continue
 
         elif now_tag == 'U':
-            chunk_postions.append([abs_p1,abs_p1])
+            chunk_postions.append([abs_p1, abs_p1])
             chunk_scores.append(score_list[abs_p1])
             abs_p1 += 1
             continue
@@ -252,7 +281,7 @@ def get_chunk_positions(tag_list, score_list=None):
             if len(main_list) == 1:
                 p1_offset = 1
             for p1_offset in range(1, len(main_list)):
-                if main_list[p1_offset] not in ['I','E']:
+                if main_list[p1_offset] not in ['I', 'E']:
                     break
                 if main_list[p1_offset] == 'E':
                     pe = abs_p1 + p1_offset
@@ -261,8 +290,8 @@ def get_chunk_positions(tag_list, score_list=None):
                 if main_list[p1_offset] == 'I':
                     continue
             if flag and pe >= ps:
-                chunk_postions.append([ps,pe])
-                chunk_scores.append(np.mean(score_list[ps:pe+1]))
+                chunk_postions.append([ps, pe])
+                chunk_scores.append(np.mean(score_list[ps:pe + 1]))
             abs_p1 += p1_offset
-            
+
     return np.array(chunk_postions), np.array(chunk_scores)
--- preprocess.py
+++ preprocess.py
@@ -19,43 +19,49 @@ from config import preprocess_folder, MODEL_DIR
 
 new_preprocess_folder = './new_cached_features'
 
-
 logger = logging.getLogger()
+
 
 # ----------------------------------------------------------------------------------------
 # Setting parser
 # ----------------------------------------------------------------------------------------
 def add_preprocess_opts(parser):
-
     # source dir
     parser.add_argument('--source_dataset_dir', type=str, default='/home/sunsi/dataset/OpenKP',
                         help="The path to the source data (raw json).")
     parser.add_argument('--output_path', type=str, default=new_preprocess_folder,
                         help="The dir to save preprocess data")
-    parser.add_argument("--pretrain_model_path", type=str, default=MODEL_DIR,
-                       help="Path to pre-trained model .")
+    parser.add_argument("--pretrain_model_path", type=str, default=MODEL_DIR,  # MODEL_DIR = "./DATA/pretrain_model/"
+                        help="Path to pre-trained model .")
     parser.add_argument("--cache_folder", type=str, default='bert-base-cased',
-                       help="pretrined tokenizer folder path")
+                        help="pretrined tokenizer folder path")
+
 
 # ----------------------------------------------------------------------------------------
 # load openkp source datasets
 # ----------------------------------------------------------------------------------------
 def set_logger(args):
-    logger = logging.getLogger() 
+    logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
     fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
-    console = logging.StreamHandler() 
-    console.setFormatter(fmt) 
+    console = logging.StreamHandler()
+    console.setFormatter(fmt)
 
     logfile = logging.FileHandler(args.log_file, 'w')
     logfile.setFormatter(fmt)
     logger.addHandler(logfile)
     logger.info('COMMAND: %s' % ' '.join(sys.argv))
 
-    
+
 def load_json_data(mode):
-    ''' Keys : 'url', 'VDOM', 'text', 'KeyPhrases' '''
+    '''
+    Keys:
+        'url', 'VDOM', 'text', 'KeyPhrases'
+
+    opt.source_dataset_dir:
+        /DATA2/wangyongbo/ms_openkp/data
+    '''
     source_path = os.path.join(opt.source_dataset_dir, 'OpenKP%s.jsonl' % mode)
     data_pairs = []
     with codecs.open(source_path, "r", "utf-8") as corpus_file:
@@ -64,16 +70,16 @@ def load_json_data(mode):
             data_pairs.append(json_)
     return data_pairs
 
+
 # ----------------------------------------------------------------------------------------
-# tokenize through space , delet null keyphrase
+# tokenize through space , delete null keyphrase
 # ----------------------------------------------------------------------------------------
 def tokenize_source_data(examples):
-    
     ''' delete null keyphrases & check are there the null keyprhases
     '''
     return_pairs = []
     shuffler = prepro_utils.DEL_ASCII()
-    for idx, ex in enumerate(tqdm(examples)):
+    for idx, ex in enumerate(tqdm(examples)):  # examples format, [{}, {}, ...]
         # tokenize :delete double spaces
         src_tokens = [w.strip() for w in ex['text'].split() if shuffler.do(w.strip())]
         # delete punctuation in the tail of keyphrase
@@ -84,9 +90,22 @@ def tokenize_source_data(examples):
             clean_phrase = ' '.join(phrase).strip('.').strip(',').strip('?').strip()
             trgs_tokens.append(clean_phrase.split())
             # trgs_tokens = [p for p in ex['KeyPhrases'] if len(p) > 0]
+
+        """
+        return_pairs:
+            [
+                [
+                    [w1, w2, w3, ...], 
+                    [[kp_w1, kp_w2, ...], ...], 
+                    'http://...'
+                ], 
+                ...
+            ]
+        """
         return_pairs.append((src_tokens, trgs_tokens, ex['url']))
 
     return return_pairs
+
 
 # ----------------------------------------------------------------------------------------
 # find keyphrase position in document and merge the same keyphrases
@@ -95,48 +114,77 @@ def filter_absent_phrase(examples):
     '''
     delete keyphrases punctuation
     find keyphrase start and end positions in documents
+
+    examples:
+        [
+            [
+                [w1, w2, w3, ...],
+                [[kp_w1, kp_w2, ...], ...],
+                'http://...'
+            ],
+            ...
+        ]
     '''
     data_list = []
     overlap_num = 0
-    
-    null_index = []
-    absent_index = []
+
+    null_index = []  # all `keyphrase` not found in current text.
+    absent_index = []  # part of `keyphrase` not found in current text.
     for idx, ex in enumerate(tqdm(examples)):
+
+        """
+        `lower_tokens`: 
+            [w1, w2, w3, ...]
         
+        `lower_phrases` (lower), `prepro_phrases` (raw) format :
+            [[kp_w1, kp_w2, ...], ...]
+        """
         lower_tokens = [t.lower() for t in ex[0]]
         lower_phrases, prepro_phrases = prepro_utils.merge_same(ex[1])
         # lower_phrases : lower cased phrases & prepro_phrases : cased phrases
-        
+
+        """
+        `present_phrases`:
+            {
+                'keyphrases': [answer1 str, answer2 str, ...], 
+                'start_end_pos': [ [[s1, e1], [s2, e2], ...], [[s1, e1], [s2, e2], ...], ... ]
+            }
+        """
         present_phrases = prepro_utils.find_answer(lower_tokens, lower_phrases)
-        if present_phrases is None:
+        if present_phrases is None:  # not found corresponding keyphrase in current text.
             null_index.append(idx)
             continue
         if len(present_phrases['keyphrases']) != len(lower_phrases):
             absent_index.append(idx)
-        
+
+        """
+        `flatten_postions`, 
+            [[s1, e1], [s2, e2], ..., [s2_1, e2_1], [s2_2, e2_2], ...]
+        """
         # filter overlap label positions
         flatten_postions = [pos for poses in present_phrases['start_end_pos'] for pos in poses]
         sorted_positions = sorted(flatten_postions, key=lambda x: x[0])
-        
+
         filter_positions = prepro_utils.filter_overlap(sorted_positions)
         if len(filter_positions) != len(sorted_positions):
             overlap_num += 1
-            
+
         data = {}
         data['url'] = ex[-1]
         data['doc_tokens'] = ex[0]
         data['keyphrases'] = prepro_phrases
         data['start_end_pos'] = filter_positions
         data_list.append(data)
-        
-    logger.info('%d sample is null, null_index as follow : '% len(null_index))
+
+    logger.info('%d sample is null, null_index as follow : ' % len(null_index))
     logger.info(null_index)
-    logger.info('%d sample is absent, absent_index as follow : '% len(absent_index))
+    logger.info('%d sample is absent, absent_index as follow : ' % len(absent_index))
     logger.info(absent_index)
-    logger.info('delete overlap keyphrase : %d , overlap / total = %.2f' 
-          %(overlap_num, float(overlap_num/len(data_list)*100)) + '%')
-        
+    logger.info('delete overlap keyphrase : %d , overlap / total = %.2f'
+                % (overlap_num, float(overlap_num / len(data_list) * 100)) + '%')
+
     return data_list
+
 
 # ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
@@ -168,30 +216,30 @@ def convert_for_bert_tag(examples, tokenizer):
             if s == e:
                 label[s] = 'U'
 
-            elif (e-s) == 1:
+            elif (e - s) == 1:
                 label[s] = 'B'
                 label[e] = 'E'
 
-            elif (e-s) >=2:
+            elif (e - s) >= 2:
                 label[s] = 'B'
                 label[e] = 'E'
-                for i in range(s+1, e):
+                for i in range(s + 1, e):
                     label[i] = 'I'
             else:
                 logger.info('ERROR')
                 break
 
         InputFeatures = {}
-        InputFeatures['url'] =  example['url']
-        InputFeatures['ex_id'] = example_index # yes
-        InputFeatures['tokens'] = all_doc_tokens # yes
+        InputFeatures['url'] = example['url']
+        InputFeatures['ex_id'] = example_index  # yes
+        InputFeatures['tokens'] = all_doc_tokens  # yes
         InputFeatures['label'] = label
         InputFeatures['valid_mask'] = valid_mask
         InputFeatures['tok_to_orig_index'] = tok_to_orig_index
         InputFeatures['orig_tokens'] = example['doc_tokens']
-        InputFeatures['orig_phrases'] = example['keyphrases'] # yes
-        InputFeatures['orig_start_end_pos'] = example['start_end_pos'] # yes
-        
+        InputFeatures['orig_phrases'] = example['keyphrases']  # yes
+        InputFeatures['orig_start_end_pos'] = example['start_end_pos']  # yes
+
         # verify InputFeatures
         if not prepro_utils.verify_ex(InputFeatures):
             continue
@@ -205,7 +253,7 @@ def convert_for_bert_tag(examples, tokenizer):
 # convert Eval examples for Test
 def preprocess_for_Eval(examples, tokenizer):
     ''' main preprocess function & verify '''
-    
+
     Features = []
     shuffler = prepro_utils.DEL_ASCII()
     for (example_index, example) in enumerate(tqdm(examples)):
@@ -225,24 +273,25 @@ def preprocess_for_Eval(examples, tokenizer):
                     valid_mask.append(1)
                 else:
                     valid_mask.append(0)
-                    
+
         InputFeatures = {}
-        InputFeatures['ex_id'] = example_index 
-        InputFeatures['url'] =  example['url']
+        InputFeatures['ex_id'] = example_index
+        InputFeatures['url'] = example['url']
         InputFeatures['VDOM'] = example['VDOM']
         InputFeatures['orig_tokens'] = doc_tokens
-        
-        InputFeatures['tokens'] = all_doc_tokens 
+
+        InputFeatures['tokens'] = all_doc_tokens
         InputFeatures['valid_mask'] = valid_mask
         InputFeatures['tok_to_orig_index'] = tok_to_orig_index
-        
+
         # verify InputFeatures
         if not prepro_utils.check_tokenize(InputFeatures):
-            logger.info('Error Found : ex_id = %d , url = %s' %(example_index, example['url']))
+            logger.info('Error Found : ex_id = %d , url = %s' % (example_index, example['url']))
             continue
         else:
             Features.append(InputFeatures)
     return Features
+
 
 # ----------------------------------------------------------------------------------------
 # save data
@@ -255,37 +304,51 @@ def save_Dev_truths(examples, filename):
             data['KeyPhrases'] = ex['KeyPhrases']
             f_pred.write("{}\n".format(json.dumps(data)))
         f_pred.close()
-    print('Success save Dev_reference to %s'% filename)
-    
+    print('Success save Dev_reference to %s' % filename)
+
+
 def save_preprocess_data(data_list, filename):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data_list, f)
     f.close()
-    
+
+
 # ----------------------------------------------------------------------------------------
 # main function
 # ----------------------------------------------------------------------------------------
 def main_preocess(input_mode, save_mode):
-    
+    """
+    opt.cache_dir:
+        ./DATA/pretrain_model/bert-base-cased
+
+    DATA:
+        OpenKPDev.jsonl
+        OpenKPEvalPublic.jsonl
+        OpenKPTrain.jsonl
+
+    :param input_mode: `EvalPublic`, `Dev`, `Train`
+    :param save_mode: `eval_public`, `valid`, `train`
+    :return:
+    """
     tokenizer = BertTokenizer.from_pretrained(opt.cache_dir)
 
-    source_data = load_json_data(input_mode)
+    source_data = load_json_data(input_mode)  # [{}, {}, ...]
     logger.info("1/5 success loaded %s data : %d " % (input_mode, len(source_data)))
-    
+
     if input_mode == 'Dev':
         save_Dev_truths(source_data, os.path.join(opt.output_path, "Dev_candidate.json"))
-        
-    # preprocess for training 
+
+    # preprocess for training
     if input_mode in ['Train', 'Dev']:
         tokenize_data = tokenize_source_data(source_data)
-        logger.info('2/5: success tokenize %s data !' %save_mode)
+        logger.info('2/5: success tokenize %s data !' % save_mode)
 
         present_data = filter_absent_phrase(tokenize_data)
         logger.info('3/5 : success obtain %s present keyphrase for training bert: %d (filter out : %d)'
-              %(save_mode, len(present_data), (len(tokenize_data) - len(present_data))))
+                    % (save_mode, len(present_data), (len(tokenize_data) - len(present_data))))
 
         return_examples = convert_for_bert_tag(present_data, tokenizer)
-        logger.info('4/5 : success obtain %s data for training bert .' %save_mode)
+        logger.info('4/5 : success obtain %s data for training bert .' % save_mode)
 
     # preprocess for evaluation
     elif input_mode == 'EvalPublic':
@@ -299,6 +362,10 @@ def main_preocess(input_mode, save_mode):
 
 
 if __name__ == "__main__":
+    """
+    Run for data preprocessing,
+        python preprocess.py --source_dataset_dir "your own directory" --output_path "your save directory"
+    """
 
     t0 = time.time()
     parser = argparse.ArgumentParser(description='preprocess_openkp.py',
@@ -308,6 +375,16 @@ if __name__ == "__main__":
     add_preprocess_opts(parser)
     opt = parser.parse_args(opt)
 
+    """
+    parser.add_argument('--source_dataset_dir', type=str, default='/home/sunsi/dataset/OpenKP',
+                        help="The path to the source data (raw json).")
+    parser.add_argument('--output_path', type=str, default=new_preprocess_folder,
+                        help="The dir to save preprocess data")
+    parser.add_argument("--pretrain_model_path", type=str, default=MODEL_DIR,  # MODEL_DIR = "./DATA/pretrain_model/"
+                        help="Path to pre-trained model .")
+    parser.add_argument("--cache_folder", type=str, default='bert-base-cased',
+                        help="pretrined tokenizer folder path")
+    """
     opt.cache_dir = os.path.join(opt.pretrain_model_path, opt.cache_folder)
     opt.log_file = os.path.join(opt.output_path, '%s-logging.txt' % time.strftime("%Y.%m.%d-%H:%M:%S"))
 
@@ -317,11 +394,10 @@ if __name__ == "__main__":
 
     if not os.path.exists(opt.output_path):
         os.makedirs(opt.output_path)
-        
+
     # set logging
     set_logger(opt)
-    
+
     mode_dir = [('EvalPublic', 'eval_public'), ('Dev', 'valid'), ('Train', 'train')]
     for input_mode, save_mode in mode_dir:
         main_preocess(input_mode, save_mode)
-
