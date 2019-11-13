7108598406ed501f276ae98aad4790627dfa1428
yongbowin <yongbowin@outlook.com>
2019-11-13 14:23:03 +0800

    add annotation to train

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
-CUDA_VISIBLE_DEVICES=2,3 python test.py --run_mode generate \
+#!/usr/bin/env bash
+export CUDA_VISIBLE_DEVICES=2,3
+
+python test.py --run_mode generate \
 --local_rank -1 \
 --pooling min \
 --eval_checkpoint ./DATA/pretrain_model/output/epoch_4.checkpoint \
 --log_dir ./Log \
---pred_dir ./Pred \
+--pred_dir ./Pred
--- train.sh
+++ train.sh
@@ -1,8 +1,11 @@
-CUDA_VISIBLE_DEVICES=2,3  python train.py --run_mode train \
+#!/usr/bin/env bash
+export CUDA_VISIBLE_DEVICES=2,3
+
+python train.py --run_mode train \
 --local_rank -1 \
 --max_train_epochs 5 \
 --save_checkpoint \
 --log_dir ./Log \
---output_dir ./output \
+--output_dir ./output
 
 # --use_viso \
